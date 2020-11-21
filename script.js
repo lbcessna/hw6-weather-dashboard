f1e3360513b9b6bd13c3eb0f7f3c84444a3b41ba
Larry Cessna <larrybcessna@gmail.com>
2020-11-21 10:45:34 -0800

    Added event listener to render data to the page and clear previous data.

--- script.js
+++ script.js
@@ -1,31 +1,47 @@
 var getCityEL = $(document.getElementById("city-name"));
+var getCitySearchEL = $(document.getElementById("city-search"));
 var getTempEL = $(document.getElementById("temperature"));
 var getHumidityEL = $(document.getElementById("humidity"));
 var getWindSpeedEL = $(document.getElementById("wind-speed"));
 
-var cityName="Denver";
-var fullStateName="Colorado";
+var cityName="";
+var fullStateName="Washington";
 var currentTemp = "";
 
+function getWeather() {
+    emptyDashFields();
 $.ajax({
     url: "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "," + fullStateName + "&appid=be2d4f56e79031b53de247f7d046faaf",
     method: "GET"
 }).then(function (response) {
     logData(response);
-    console.log("forecast", response);
     return currentTemp = kelvinToFarenheit(response.main.temp);
     
 });
+}
+
+$("#search-btn").on("click", event =>{
+    event.preventDefault();
+    console.log("User search field:", $("#city-search").val());
+    cityName = $("#city-search").val();
+    getWeather();
+})
 function logData(forecast){
     getCityEL.textContent = "";
     console.log("Forecast Info: ", forecast);
     console.log("City Name is ", forecast.name);
-    console.log("Humidity is ", forecast.main.humidity + "%");
     getHumidityEL.append(forecast.main.humidity + "%")
     getWindSpeedEL.append(forecast.wind.speed +" mph" )
 
 };
 
+function emptyDashFields () {
+    getCityEL.empty();
+    getTempEL.empty();
+    getHumidityEL.empty();
+    getWindSpeedEL.empty();
+
+}
 function kelvinToFarenheit(valNum) {
     console.log("GetTempEl: ", getTempEL);
     valNum = parseFloat(valNum);
